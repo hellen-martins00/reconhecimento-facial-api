63fb1ae9d0a4a3a844ab6dee8994789fde2954e8
Hellen Martins <martinshellen235@gmail.com>
2026-08-28 17:29:01 -0300

    Configura CORS em producao para o Railway

--- app/main.py
+++ app/main.py
@@ -28,8 +28,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "https://reconhecimento-facial-front-production.up.railway.app",
         "http://127.0.0.1:5173",
+        "http://localhost:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
